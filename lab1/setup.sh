e96079b88053ec0606b54d705560d73883d44d2c
Jason Lau <i@dotkrnl.com>
2021-01-12 22:46:35 -0800

    feat: use zip format instead of tarball

--- lab1/setup.sh
+++ lab1/setup.sh
@@ -2,8 +2,8 @@
 if [ "$(uname -s)" == 'Darwin' ]; then
   xcode-select --install
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  /usr/local/bin/brew install llvm
+  /usr/local/bin/brew install llvm zip
 else
   sudo apt update
-  sudo env DEBIAN_FRONTEND=noninteractive apt install build-essential -y
+  sudo env DEBIAN_FRONTEND=noninteractive apt install build-essential zip -y
 fi
