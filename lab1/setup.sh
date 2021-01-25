7a8018e10837746aeaa005f337a166c870374ca6
Jason Lau <i@dotkrnl.com>
2021-01-25 13:28:36 -0800

    feat: do not install brew if already exist

--- lab1/setup.sh
+++ lab1/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$(uname -s)" == 'Darwin' ]; then
   xcode-select --install
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  [ -f /usr/local/bin/brew ] || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   /usr/local/bin/brew install llvm zip
 else
   sudo apt update
